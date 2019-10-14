66c7a7b1d06c7d50c7e5e59fde1a7b1b19230605
Someone <someone19911991@gmail.com>
2019-10-14 16:08:55 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Profile from "./components/Profile";
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import MyPosts from './components/Profile/MyPosts/Posts/MyPosts';
 
 
 const App = () => {
@@ -10,7 +10,7 @@ const App = () => {
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
-            <Profile/>
+            <MyPosts/>
 
         </div>);
 }
---
+++ src/components/Profile/MyPosts/Posts/MyPosts.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import s from './MyPosts.module.css';
+
+const MyPosts = () => {
+    return(
+            <div>
+                My Posts
+                <div>
+                    New Post
+                </div>
+                <div className={s.posts}>
+                <div className={s.item}>
+                    Post 1
+                </div>
+                <div className={s.item}>
+                    Post 2
+                </div>
+            </div>
+          </div>
+    );
+}
+export default MyPosts;
