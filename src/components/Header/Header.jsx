358f8f2670862c8f4652eadd23b4578a957df4f2
Someone <someone19911991@gmail.com>
2019-11-13 11:54:58 +0400

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import s from './Header.module.css';
 
 const Header = () => {
-    return <header className={s.header}>
-        <img src='http://iguansystems.com/image/logo/Iguan6-01.png' alt={'logo'}/>
-    </header>
+    return <header className= {s.header}>
+                <img src='http://iguansystems.com/image/logo/Iguan6-01.png' alt='iguan_logo'/>
+           </header>
 }
 export default Header;
