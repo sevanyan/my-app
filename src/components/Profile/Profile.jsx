466b68f6a07b92316d43ec2fdc176c645d350944
Someone <someone19911991@gmail.com>
2019-12-16 19:09:29 +0400

    crated constant posts and equal to GetState profilepage posts

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-
-
+import {addPostActionCreator,updateNewPostTextActionCreator} from"../../redux/profile-reducer";
 
 const Profile = (props) => {
-
+   const posts =  props.store.getState().profilePage.posts;
     return(
         <div>
 
             <ProfileInfo/>
-            <MyPosts posts = {props.profilePage.posts}
+            <MyPosts posts = {posts}
                      newPostText = {props.profilePage.newPostText}
                      dispatch = {props.dispatch} />
         </div>
