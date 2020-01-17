902116b7e3be712d4019fb97475eba96d974c1bc
Someone <someone19911991@gmail.com>
2020-01-17 18:57:59 +0400

    Created moq data for user pages

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -4,22 +4,22 @@ import {connect} from "react-redux";
 import {followAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
 
 let mapStateToProps = (state) => {
-    return{
-        users:state.usersPage.users
+    return {
+        users: state.usersPage.users
     }
 }
 
-let mapDispatchToProps  = (dispatch) =>{
-    return{
-        follow: (userId) =>{
+let mapDispatchToProps = (dispatch) => {
+    return {
+        follow: (userId) => {
             dispatch(followAC(userId));
         },
-        unfollow: (userId) =>{
+        unfollow: (userId) => {
             dispatch(unfollowAC(userId));
         },
-        setUsers: (users) =>{
+        setUsers: (users) => {
             dispatch(setUsersAC(users));
         }
     }
 }
-export default connect(mapStateToProps,mapDispatchToProps(Users));
+export default connect(mapStateToProps, mapDispatchToProps)(Users);
