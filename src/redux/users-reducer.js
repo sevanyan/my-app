9a74717741d4419f9b59d146b590069a6fcfaae4
Someone <someone19911991@gmail.com>
2020-01-23 19:02:00 +0400

    Created moq data for user pages

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -45,4 +45,4 @@ export const usersReducer = (state = initialState, action) => {
 export const followAC = (userId) => ({type: FOLLOW, userId});
 export const unfollowAC = (userId) => ({type: UNFOLLOW, userId});
 export const setUsersAC = (users) => ({type: SET_USERS, users});
-export default usersReducer;
+export default usersReducer ;
