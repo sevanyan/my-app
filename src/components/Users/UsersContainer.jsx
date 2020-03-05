8ebda0c1a9340560a4fe10a672a1ff0d87008c44
Someone <someone19911991@gmail.com>
2020-03-05 19:09:54 +0400

    1. Created Header Container ta make it safety 2. Created auth reducer for login

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -11,7 +11,10 @@ import Preloader from "../../Common/Preloader/preloader";
 class UsersContainer extends React.Component {
     componentDidMount() {
         this.props.toggleIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count = ${this.props.pageSize} `)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count = ${this.props.pageSize}`,
+            {
+                withcridentials: true
+            })
             .then(response => {
                 this.props.toggleIsFetching(false);
                 this.props.setUsersTotalCount(response.data.totalCount);
@@ -22,7 +25,10 @@ class UsersContainer extends React.Component {
     onPageChanged = (pageNumber) => {
         this.props.setCurrentPage(pageNumber);
         this.props.toggleIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count = ${this.props.pageSize} `)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count = ${this.props.pageSize} `,
+        {
+            withcridentials: true
+        })
             .then(response => {
                 this.props.setUsers(response.data.items);
                 this.props.toggleIsFetching(false);
